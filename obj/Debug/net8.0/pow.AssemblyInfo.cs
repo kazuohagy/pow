fd59fb49931ad027e95f2442a303c412beafc1bd
Gustavo Kazuo <kazucalado@gmail.com>
2024-10-28 11:37:42 -0300

    quale andromeda

--- obj/Debug/net8.0/pow.AssemblyInfo.cs
+++ obj/Debug/net8.0/pow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9e4bf1393b66db440d1303ba63159b3ad72cb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03983a127a7ffc6702c0653e2ee198272710b1a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("pow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
