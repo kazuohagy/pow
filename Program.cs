69d046583cf2040cbbad890b01c7910f62adea94
Gustavo Kazuo <kazucalado@gmail.com>
2024-10-29 14:01:36 -0300

    [Program.cs] classe modificada para o blockchain

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Transaction transaction = new Transaction("Rafael", "Thamires", 10000);
+
+        //converter a transacao para uma string (em uma aplicacao real, isso seria feito de forma mais sofisticada)
+        string transactionData = $"{transaction.Sender} -> {transaction.Recipient} : {transaction.Amount}";
+
+        int nonce = ProofOfWork.FindNonce(transactionData, "00");
+
+        Console.WriteLine($"Nonce encontrado: {nonce}");
+    }
+}
